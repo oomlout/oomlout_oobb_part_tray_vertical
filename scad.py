b9664cf81f7f5f9c71749c813f6e74cf925ecc5e
Oom Lout <aaron@oomlout.com>
2025-03-29 00:27:08 +0000

    working

--- scad.py
+++ scad.py
@@ -28,9 +28,9 @@ def make_scad(**kwargs):
         #no overwrite and filter
         filter = ""
         #filter = """
-        #filter = filter; save_type = "all"; navigation = True; overwrite = False; modes = ["3dpr"]; oomp_run = True
+        filter = filter; save_type = "all"; navigation = True; overwrite = False; modes = ["3dpr"]; oomp_run = True
         #default
-        filter = ""; save_type = "all"; navigation = True; overwrite = True; modes = ["3dpr"]; oomp_run = True
+        #filter = ""; save_type = "all"; navigation = True; overwrite = True; modes = ["3dpr"]; oomp_run = True
     elif typ == "fast":
         filter = ""; save_type = "none"; navigation = True; overwrite = True; modes = ["3dpr"]; oomp_run = False
         #default
